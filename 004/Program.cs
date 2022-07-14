0c81d506c86a26f47176dd5e803bd7db36abee25
Olya <dolcheveleno@mail.ru>
2022-07-14 13:33:05 +0300

    zadacha 4

---
+++ 004/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+bool S1 = n <= 0;
+if (S1)
+{
+    Console.WriteLine("Операция не выполнена");
+}
+for (int i=a; i<=n; i++)
+{
+    bool S2 = i%2 == 0;
+    if (S2)
+    {
+         Console.Write(i+ " ");
+    }
+}
+Console.WriteLine();
