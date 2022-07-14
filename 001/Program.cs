7e063010929b7a03f30104f1c85ab5524019d8cd
Olya <dolcheveleno@mail.ru>
2022-07-14 12:53:21 +0300

    zadacha 1

--- 001/Program.cs
+++ 001/Program.cs
@@ -8,11 +8,18 @@ Console.WriteLine("Введите первое число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int b = Convert.ToInt32(Console.ReadLine());
-if (a > b)
+bool S1 = a > b;
+bool S2 = a == b;
+bool S3 = b > a;
+if (S1)
 {
     Console.WriteLine($"max = {a}");
 }
-else 
+else if (S2)
+{
+    Console.WriteLine("Числа равны");
+}
+if (S3)
 {
     Console.WriteLine($"max = {b}");
 }
